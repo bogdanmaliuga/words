0aae1ae50687b0364df73d4b424f1d5236e0e840
Bohdan-Ivan Maliuha <bogdan.maliuga@gmail.com>
2017-12-13 09:55:12 +0200

    test

--- renderer.js
+++ renderer.js
@@ -1,3 +1,102 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const app = require('electron').remote;
+const dialog = app.dialog;
+const fs = require('fs');
+const dir = require('node-dir');
+let $ = require('jquery')
+let data = [];
+let dataPoints=[];
+let index = 0;
+
+let options = {
+    exportEnabled: true,
+    animationEnabled: true,
+    title: {
+        text: "jQuery Spline Area Chart"
+    },
+    data: [{
+		type: "spline",
+		dataPoints: [
+		]
+	}]
+};
+
+
+function compare(a,b) {
+    if (a.x < b.x)
+      return -1;
+    if (a.x > b.x)
+      return 1;
+    return 0;
+}
+function countWords(s) {
+    s = s.replace(/(^\s*)|(\s*$)/gi, "");
+    s = s.replace(/[ ]{2,}/gi, " ");
+    s = s.replace(/\n /, "\n");
+    return s.split(' ').length;
+}
+
+function readFilesFromDir(path) {
+    index = 0;
+    $('.loader-wrap').css('display', 'block');
+    data = [];
+    dataPoints=[];
+    $("#myTable > tbody:last").children().empty();
+    dir.readFiles(path, {
+        match: /.txt$/,
+        exclude: /^\./
+    }, function (err, content, filename, next) {
+        if (err) {
+            $('.loader-wrap').css('display', 'none');
+            dialog.showErrorBox('Помилка', err.toString());
+            throw err
+        };
+        index++;
+        $('.count').text(index.toString());
+        $('#myTable tr:last').after('<tr><td>' +
+            filename +
+            '</td><td>' +
+            +fs.statSync(filename).size +
+            '</td><td>' +
+            countWords(content) +
+            '</td></tr>');
+        dataPoints.push({
+            x:countWords(content),
+            y:fs.statSync(filename).size,
+        })
+        data.push({
+            name: filename,
+            size: fs.statSync(filename).size,
+            text: content,
+            count: countWords(content)
+        })
+        next();
+    },
+        function (err, files) {
+            if (err) {
+                dialog.showErrorBox('Помилка', err.toString());
+                $('.loader-wrap').css('display', 'none')
+                throw err
+            };
+            console.log('finished reading files:');
+            dataPoints.sort(compare)
+            options.data[0].dataPoints=dataPoints;
+            console.log(options);
+            $("#chartContainer").CanvasJSChart(options);
+            $('.loader-wrap').css('display', 'none')
+        });
+}
+
+
+$('#chooseFolder').on('click', function () {
+    dialog.showOpenDialog({
+        properties: ['openDirectory']
+    }, function (filename) {
+        if (filename) {
+            $('#path').text(filename.toString());
+            readFilesFromDir(filename.toString());
+        } else {
+            $('#path').text('Папку не вибрано');
+
+        }
+    });
+})
